458a5c8ce1430bdc1e75403f3a6552fef5390c4e
Ritik kumar <149752140+rk-ritik-raj@users.noreply.github.com>
2026-03-17 15:44:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,62 @@
+// import express from "express"
+// import dotenv from "dotenv"
+// dotenv.config()
+// import connectDb from "./config/db.js"
+// import authRouter from "./routes/auth.routes.js"
+// import cors from "cors"
+// import cookieParser from "cookie-parser"
+// import userRouter from "./routes/user.routes.js"
+// import geminiResponse from "./gemini.js"
+
+
+// const app=express()
+// app.use(cors({
+//   origin: [
+//     "http://localhost:5173",
+//     "https://virtualassistant-bjti.onrender.com"
+//   ],
+//   credentials: true
+// }))
+// const port=process.env.PORT || 5000
+// app.use(express.json())
+// app.use(cookieParser())
+// app.use("/api/auth",authRouter)
+// app.use("/api/user",userRouter)
+
+
+// app.listen(port,()=>{
+//     connectDb()
+//     console.log("server started")
+// })
+
+
 import express from "express"
 import dotenv from "dotenv"
-dotenv.config()
-import connectDb from "./config/db.js"
-import authRouter from "./routes/auth.routes.js"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import connectDb from "./config/db.js"
+import authRouter from "./routes/auth.routes.js"
 import userRouter from "./routes/user.routes.js"
-import geminiResponse from "./gemini.js"
 
+dotenv.config()
 
-const app=express()
+const app = express()
+
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://virtualassistant-bjti.onrender.com"
-  ],
+  origin: true,
   credentials: true
 }))
-const port=process.env.PORT || 5000
+
 app.use(express.json())
 app.use(cookieParser())
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
 
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
 
-app.listen(port,()=>{
-    connectDb()
-    console.log("server started")
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  connectDb()
+  console.log("Server started")
 })
 
