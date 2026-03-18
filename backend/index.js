02549c779106d3382331648a3097338d6379cbf9
Ritik kumar <149752140+rk-ritik-raj@users.noreply.github.com>
2026-03-18 13:44:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,26 @@
 import express from "express"
 import dotenv from "dotenv"
-dotenv.config()
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 import connectDb from "./config/db.js"
 import authRouter from "./routes/auth.routes.js"
-import cors from "cors"
-import cookieParser from "cookie-parser"
 import userRouter from "./routes/user.routes.js"
+
+dotenv.config()
 
 const app = express()
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://virtualassistant-bjti.onrender.com"
-  ],
+  origin: "https://virtualassistant-bjti.onrender.com",
   credentials: true
 }))
 
 app.use(express.json())
 app.use(cookieParser())
 
-app.use("/api/auth", authRouter)
-app.use("/api/user", userRouter)
+app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
 
 const port = process.env.PORT || 5000
 
