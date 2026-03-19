b1c58896ce7e4af7f951fc43e6df79b8ef0d0c43
Ritik kumar <149752140+rk-ritik-raj@users.noreply.github.com>
2026-03-20 04:36:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,28 @@
 import express from "express"
 import dotenv from "dotenv"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-
+dotenv.config()
 import connectDb from "./config/db.js"
 import authRouter from "./routes/auth.routes.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 import userRouter from "./routes/user.routes.js"
+import geminiResponse from "./gemini.js"
 
-dotenv.config()
 
-const app = express()
-
+const app=express()
 app.use(cors({
- origin:true,
- credentials:true
+    origin:"http://localhost:5173",
+    credentials:true
 }))
-
+const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
+app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
 
-app.get("/",(req,res)=>{
- res.send("API running")
+
+app.listen(port,()=>{
+    connectDb()
+    console.log("server started")
 })
 
-app.use("/api/auth", authRouter)
-app.use("/api/user", userRouter)
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, async()=>{
- await connectDb()
- console.log("server running")
-})
