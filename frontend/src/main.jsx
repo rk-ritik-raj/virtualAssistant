b16debec11414019cf70af1ea114a3a94901123f
Ritik kumar <149752140+rk-ritik-raj@users.noreply.github.com>
2026-03-20 04:37:04 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,19 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom"
+import {BrowserRouter} from "react-router-dom"
 import UserContext from './context/UserContext.jsx'
-import axios from "axios"
-
-axios.defaults.withCredentials = true
-axios.defaults.baseURL = "https://virtualassistant-backend-zaoi.onrender.com"
-
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <UserContext>
-        <App />
-      </UserContext>
-    </BrowserRouter>
-  </StrictMode>
+<BrowserRouter>
+<UserContext>
+    <App />
+  </UserContext>
+  </BrowserRouter>
+ 
 )
